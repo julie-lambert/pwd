3e615c4f40a330cf256a56085d91500622b4cdb3
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2024-01-30 14:51:43 +0100

    INDEX.php update

--- index.php
+++ index.php
@@ -1,55 +1,10 @@
-<?php
-
-require_once 'vendor/autoload.php';
-
-use App\Model\Clothing as Clothing;
-use App\Model\Electronic as Electronic;
-
-
-
-$clothing = new Clothing();
-$electronic = new Electronic();
-
-var_dump($clothing);
-var_dump($electronic);
-
-
-// findOneById -------------------------
-// $newClothing = $clothing->findOneById(1);
-// $newElectronic = $electronic->findOneById(9);
-
-// findAll -------------------------
-// $newClothing = $clothing->findAll();
-// $newElectronic = $electronic->findAll();
-
-// create -------------------------
-// $clothing->create();
-// $electronic->create();
-
-// update -------------------------
-// $clothing->update();
-// $electronic->update();
-
-
-// var_dump($newClothing);
-// var_dump($newElectronic);
-
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Job15</title>
+    <title>My Little MVC</title>
 </head>
 
-<body>
-    <h1>Job15</h1>
-</body>
-
-</html>
+<a href="./shop.php">SHOP</a>
