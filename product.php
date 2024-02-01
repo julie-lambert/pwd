5c5ac15abc66d26a6310d0b2442c62f6ddafdb67
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2024-02-01 16:36:28 +0100

    Refactor code for displaying product details and implementing authentication

--- product.php
+++ product.php
@@ -1,55 +1,40 @@
 <?php
 
 require_once 'vendor/autoload.php';
+session_start();
 
-use App\Model\Clothing;
-use App\Model\Electronic;
-use App\Model\Product;
+use App\Controller\ShopController;
 
-//Si la clé id_product existe dans l'url et que sa valeur est un int
-if (isset($_GET['id_product']) && ctype_digit($_GET['id_product'])) {
-  //On récupère la valeur de la clé id_product
-  $idProduct = $_GET['id_product'];
-  //On instancie un objet de la classe Clothing ou Electronic en fonction de la valeur de la clé product_type
-  if ($_GET['product_type'] === 'clothing') {
-    $product = new Clothing();
-  } elseif ($_GET['product_type'] === 'electronic') {
-    $product = new Electronic();
-  } else {
-    $product = new Product();
-    $horsType= "Ce produit est hors catégorie";
-  }
-}
-
-$finalProduct = $product->findOneById($idProduct);
-//On récupere le produit avec le findOneById de la classe Product
+$finalProduct = (new ShopController())->showProduct($_GET['id_product'], $_GET['product_type']);
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
-  
-  <?php if ($finalProduct) :?>
+<html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $finalProduct->getName() ?></title>
+  <title>Produit</title>
 </head>
+
 <body>
-  <h1><?= $finalProduct->getName() ?></h1>
-  <?php if(isset($horsType)): ?>
-    <h3><?= $horsType ?></h3>
+  <?php if ($finalProduct) : ?>
+    <?php if ($finalProduct == "Vous devez être connecté pour accéder à cette page") : ?>
+      <h2><?= $finalProduct ?>, vous allez être redirigé</h2>
+    <?php else : ?>
+      <h1><?= $finalProduct->getName() ?></h1>
+      <p><?= $finalProduct->getDescription() ?></p>
+      <p><?= $finalProduct->getPrice() ?> €</p>
+      <p><?= $finalProduct->getQuantity() ?></p>
+      <?php foreach ($finalProduct->getPhotos() as $photo) : ?>
+        <img src="<?= $photo ?>" alt="photo de <?= $finalProduct->getName() ?>">
+      <?php endforeach; ?>
+    <?php endif; ?>
+  <?php else : ?>
+    <h2>Le produit demandé n'est pas disponible</h2>
   <?php endif; ?>
-  <p><?= $finalProduct->getPrice() ?> €</p>
-  <p><?= $finalProduct->getDescription() ?></p>
-  <p><?= $finalProduct->getQuantity() ?></p>
-  <p><?= $finalProduct->getCategory_id() ?></p>
-  <div id="slider">
-    <?php foreach ($finalProduct->getPhotos() as $photo) : ?>
-      <img src="<?= $photo ?>" alt="" height=340 width=340>
-    <?php endforeach; ?>
-  </div>
-  <?php else: ?>
-    <p>Le produit demandé n'est pas disponible</p>
-  <?php endif; ?>
+
 </body>
+
+
 </html>
--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -1,14 +1,17 @@
 <?php
 
 namespace App\Controller;
+
 use App\Model\Product;
 use App\Model\Clothing;
 use App\Model\Electronic;
+use App\Controller\AuthenticationController;
 
 
-class ShopController{
+class ShopController
+{
 
-    public function index(int $page): array 
+    public function index(int $page): array
     {
         $AllProducts = new Product();
         return $AllProducts->findPaginated($page);
@@ -16,18 +19,40 @@ class ShopController{
 
     public function productType(int $id): string|false
     {
-        
+
         $clothing = new Clothing();
         $electronic = new Electronic();
         $resultClothing = $clothing->findOneById($id);
         $resultElectronic = $electronic->findOneById($id);
 
-        if($resultClothing){
+        if ($resultClothing) {
             return "clothing";
-        }elseif($resultElectronic){
+        } elseif ($resultElectronic) {
             return "electronic";
-        }else{
+        } else {
             return false;
+        }
+    }
+
+    public function showProduct($id_product, $productType): object|string
+    {
+        $auth = new AuthenticationController();
+        if ($auth->profile()) {
+            $auth->profile();
+            $clothing = new Clothing();
+            $electronic = new Electronic();
+            $product = new Product();
+            if ($productType == "clothing") {
+                return $clothing->findOneById($id_product);
+            } elseif ($productType == "electronic") {
+                return $electronic->findOneById($id_product);
+            } else {
+                return $product->findOneById($id_product);
+            }
+        } else {
+            $message = "Vous devez être connecté pour accéder à cette page";
+            header("refresh:3; url=./login.php");
+            return $message;
         }
     }
 }
