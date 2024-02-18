2b05a1ac9d47cfc724afc68cec439d93bdec7b74
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2024-02-18 21:06:34 +0100

    ADD => On click on miniatures, swiper active

---
+++ scripts/productScript.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let sections = document.querySelectorAll("section");
+  let main = document.querySelector("main");
+
+  sections.forEach((section) => {
+    let swiper = new Swiper(section.querySelector(".swiper"), {
+      loop: true,
+      slidesPerView: 1,
+      pagination: {
+        el: section.querySelector(".swiper-pagination"),
+        clickable: true,
+      },
+      centeredSlides: true,
+      navigation: {
+        nextEl: section.querySelector(".button-right"),
+        prevEl: section.querySelector(".button-left"),
+      },
+    });
+
+    if (window.innerWidth >= 1024) {
+      let photos = section.querySelectorAll(".photos > div");
+      let dots = section.querySelectorAll(".swiper-pagination span");
+
+      const activeThumb = (current) => {
+        section.querySelector(".photos > .active").classList.remove("active");
+        current.classList.add("active");
+      };
+
+      photos.forEach((photo, i) => {
+        photo.addEventListener("click", () => {
+          activeThumb(photo);
+          dots[i].click();
+        });
+      });
+
+      swiper.on("realIndexChange", (swiper) => {
+        activeThumb(photos[swiper.realIndex]);
+      });
+    }
+  });
+});
