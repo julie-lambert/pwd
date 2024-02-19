5533b30a7cf0cc28ab0169b0c2a22b96850b43fc
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2024-02-19 09:37:09 +0100

    ADD => renaming page header to index.php and delete the other index.php

--- index.php
+++ index.php
@@ -1,15 +1,61 @@
 <?php
-//Redirection vers la page shop.php
-header("Location: ./shop.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Little MVC</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Header</title>
+  <link rel="stylesheet" href="./assets/css/index.css">
 </head>
 
-<a href="./shop.php">SHOP</a>
+<body>
+  <div class="container">
+    <nav>
+      <div>
+        <a href="/index.php">Home</a>
+        <a href="/shop.php">Shop</a>
+      </div>
+      <div>
+        <a href="/profile.php">Profil</a>
+        <a href="/register.php">Inscription/Connexion</a>
+      </div>
+    </nav>
+  </div> 
+  <h1 class="counter">
+    0
+  </h1>
+  <div class="overlay">
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+  </div>
+
+
+
+  <div class="header">
+    <div class="h1">M</div>
+    <div class="h1">L</div>
+    <div class="h1">M</div>
+    <div class="h1">V</div>
+    <div class="h1">C</div>
+    <div class="h1">.</div>
+  </div>
+  <div class="hero">
+    <img src="./assets/img/bg4.jpg" alt="">
+  </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="./scripts/headerScript.js"></script>
+</body>
+
+</html>
