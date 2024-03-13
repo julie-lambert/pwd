fbe6e60680610e481ac0a22aef803a3396dfc148
thomas-spinec <thomas.spinec@laplateforme.io>
2024-03-13 11:16:48 +0100

    Update header.php to use BASE_DIR environment variable for CSS file path

--- src/view/header.php
+++ src/view/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/pwd/assets/css/header.css">
+  <link rel="stylesheet" href="<?= $_ENV['BASE_DIR'] ?>/assets/css/header.css">
 </head>
 
 <body>
